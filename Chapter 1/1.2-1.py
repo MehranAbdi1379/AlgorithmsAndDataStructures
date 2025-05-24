636e3b904a6cad2747dd0f6b490db44a117b89d6
Mehran Abdi <mehranabdi13793@gmail.com>
2025-05-24 10:33:43 +0330

    Solve Palindrome Checker

---
+++ Chapter 1/1.2-1.py
@@ -0,0 +1,17 @@
+﻿text = input("Please enter the text: ")
+
+polishedText = ""
+
+for char in text:
+    if char.isalpha():
+        polishedText += char.lower()
+
+reversedText = ""
+
+for i in range(len(polishedText) - 1, -1, -1):
+    reversedText += polishedText[i]
+
+if reversedText == polishedText:
+    print("Yes")
+else:
+    print("No")
