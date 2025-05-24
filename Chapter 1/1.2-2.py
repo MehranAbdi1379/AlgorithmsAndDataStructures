14fcb14727ca5fec6c486955902a4a445c602816
Mehran Abdi <mehranabdi13793@gmail.com>
2025-05-24 10:39:36 +0330

    Solve FizzBuzz with a twist

---
+++ Chapter 1/1.2-2.py
@@ -0,0 +1,16 @@
+﻿for i in range(1, 51):
+    isPrime = True
+    for j in range(2, i):
+        if i % j == 0:
+            isPrime = False
+            break
+    if i > 1 and isPrime:
+        print("Prime")
+    elif i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
