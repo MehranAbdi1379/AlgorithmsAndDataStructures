c1eadc5ce34756fca32fa02c3bfd7bc9786d09ee
Mehran Abdi <mehranabdi13793@gmail.com>
2025-05-24 10:45:22 +0330

    Solve Sum of Digits Until One Digit

---
+++ Chapter 1/1.2-3.py
@@ -0,0 +1,8 @@
+﻿input_number = input("Enter a number: ")
+
+while len(input_number) > 1:
+    sum = 0
+    for num in input_number:
+        sum += int(num)
+    input_number = str(sum)
+print(input_number)
