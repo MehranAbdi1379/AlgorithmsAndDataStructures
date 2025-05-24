db77b7adffed7ea757f2c43c6eb29ab089f37f08
Mehran Abdi <mehranabdi13793@gmail.com>
2025-05-24 10:51:13 +0330

    Solve Two Sum Problem

---
+++ Chapter 1/1.2-4.py
@@ -0,0 +1,7 @@
+﻿nums = [2, 3, 3, 7, 11, 54, 24, 45, 3, 15]
+target = 35
+
+for i in range(len(nums) - 1):
+    for j in range(i, len(nums)):
+        if nums[i] + nums[j] == target and i != j:
+            print(str(i) + " " + str(j))
