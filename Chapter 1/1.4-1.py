2e773752054f1b87d6605f5be9da5ba8191db1f4
Mehran Abdi <mehranabdi13793@gmail.com>
2025-05-25 12:10:36 +0330

    Solve Python Flow Practice Problems

---
+++ Chapter 1/1.4-1.py
@@ -0,0 +1,21 @@
+﻿for i in range(3):
+    for j in range(3):
+        if i == j:
+            break
+        print(f"{i}-{j}")
+
+x = 0
+while x < 5:
+    x += 1
+    if x == 3:
+        continue
+    print(x)
+
+a = True
+b = False
+c = True
+
+if a and b or c:
+    print("Yes")
+else:
+    print("No")
